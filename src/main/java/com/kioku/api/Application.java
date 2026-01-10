dbb8e78e85c0445c0f6ec43994ebf9ac0b909c14
stephen-os <ImStephen.1sw@gmail.com>
2026-01-09 19:27:38 -0500

    refactor: cleaned up Application.java

--- src/main/java/com/kioku/api/Application.java
+++ src/main/java/com/kioku/api/Application.java
@@ -1,26 +1,12 @@
 package com.kioku.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.run(args);
-    }
-
-    @Bean
-    CommandLineRunner init() {
-        return args -> {
-            logger.info("Initialized successfully");
-        };
     }
 }
